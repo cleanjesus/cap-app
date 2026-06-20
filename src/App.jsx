3653c30c2d021771bafd0868f29614cc07ebcb89
Jesus <jesussantiago.edu@gmail.com>
2026-06-20 14:12:52 -0700

    finalized lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import APIForm from "../components/APIForm";
+import Gallery from "../components/Gallery";
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
@@ -12,7 +13,67 @@ function App() {
         width: "",
         height: "",
     });
-    const submitForm = () => {};
+    const [currentImage, setCurrentImage] = useState(null);
+    const [prevImages, setPrevImages] = useState([]);
+
+    const submitForm = () => {
+        let defaultValues = {
+            format: "jpeg",
+            no_ads: "true",
+            no_cookie_banners: "true",
+            width: "1920",
+            height: "1080",
+        };
+        if (inputs.url == "" || inputs.url == " ") {
+            alert("You forgot to submit an url!");
+        } else {
+            const updatedInputs = { ...inputs };
+            for (const [key, value] of Object.entries(inputs)) {
+                if (value == "") {
+                    updatedInputs[key] = defaultValues[key];
+                }
+            }
+            setInputs(updatedInputs);
+            makeQuery();
+        }
+    };
+
+    const makeQuery = () => {
+        let wait_until = "network_idle";
+        let response_type = "json";
+        let fail_on_status = "400%2C404%2C500-511";
+        let cleanURL = inputs.url.replace(/^https?:\/\//, "");
+        let fullURL = "https://" + cleanURL;
+
+        let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+
+        callAPI(query).catch(console.error);
+    };
+
+    const callAPI = async (query) => {
+        const response = await fetch(query);
+        const json = await response.json();
+        if (json.url == null) {
+            alert(
+                "Oops! Something went wrong with that query, let's try again!",
+            );
+        } else {
+            setCurrentImage(json.url);
+            setPrevImages((images) => [...images, json.url]);
+            reset();
+        }
+    };
+
+    const reset = () => {
+        setInputs({
+            url: "",
+            format: "",
+            no_ads: "",
+            no_cookie_banners: "",
+            width: "",
+            height: "",
+        });
+    };
 
     return (
         <div className="whole-page">
@@ -29,6 +90,36 @@ function App() {
                 onSubmit={submitForm}
             />
             <br></br>
+            {currentImage ? (
+                <img
+                    className="screenshot"
+                    src={currentImage}
+                    alt="Screenshot returned"
+                />
+            ) : (
+                <div> </div>
+            )}
+            <div className="container">
+                <h3> Current Query Status: </h3>
+                <p>
+                    https://api.apiflash.com/v1/urltoimage?access_key=ACCESS_KEY
+                    <br></br>
+                    &url={inputs.url} <br></br>
+                    &format={inputs.format} <br></br>
+                    &width={inputs.width}
+                    <br></br>
+                    &height={inputs.height}
+                    <br></br>
+                    &no_cookie_banners={inputs.no_cookie_banners}
+                    <br></br>
+                    &no_ads={inputs.no_ads}
+                    <br></br>
+                </p>
+            </div>
+            <br></br>
+            <div className="container">
+                <Gallery images={prevImages} />
+            </div>
         </div>
     );
 }
